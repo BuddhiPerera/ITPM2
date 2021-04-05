5cfbc03b1d26eef89293080fd3869a9bc4d78bbe
BuddhiPerera <buddhiperera27@gmail.com>
2021-04-05 19:42:24 +0530

    bla

--- timeTable.sql
+++ timeTable.sql
@@ -13,7 +13,6 @@ CREATE TABLE `WorkingDays`
     `workingTimeMinutes` int         DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-<<<<<<< HEAD
 
 CREATE TABLE `AddLecturer` (
                                `id` int NOT NULL,
@@ -40,41 +39,14 @@ CREATE TABLE `AddStudentSubGroups`(
                                       PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `AddTags`(
-                                      `id` int NOT NULL,
-                                      `tagName` varchar(20) DEFAULT NULL,
-                                      `tagCode` int  DEFAULT NULL,
-                                      `relatedTag` varchar(30) DEFAULT NULL,
-                                      PRIMARY KEY (`id`)
-=======
-CREATE TABLE `AddLecturer`
-(
-    `id`         int          NOT NULL,
-    `empId`      varchar(50)  NOT NULL,
-    `lName`      varchar(50)  NOT NULL,
-    `department` varchar(60)  NOT NULL,
-    `faculty`    varchar(40)  NOT NULL,
-    `center`     varchar(40)  NOT NULL,
-    `buildingNO` varchar(20)  NOT NULL,
-    `level`      varchar(70)  NOT NULL,
-    `rank`       varchar(100) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `AddStudentSubGroups`
+CREATE TABLE `AddTags`
 (
     `id`         int NOT NULL,
-    `year`       int          DEFAULT NULL,
-    `semester`   int          DEFAULT 1,
-    `programme`  varchar(20)  DEFAULT NULL,
-    `groupNo`    int          DEFAULT NULL,
-    `subGroupNo` int          DEFAULT NULL,
-    `groupId`    varchar(100) DEFAULT NULL,
-    `subGroupId` varchar(100) DEFAULT NULL,
+    `tagName`    varchar(20) DEFAULT NULL,
+    `tagCode`    int         DEFAULT NULL,
+    `relatedTag` varchar(30) DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
 
 CREATE TABLE `AddLocations`
 (
@@ -85,5 +57,4 @@ CREATE TABLE `AddLocations`
     `laboratory`   boolean     DEFAULT 0,
     `Capacity`     char(10)    DEFAULT NULL,
     PRIMARY KEY (`id`)
->>>>>>> ef236bbb28e6a7e1f593da11449bca935bb2ada6
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
