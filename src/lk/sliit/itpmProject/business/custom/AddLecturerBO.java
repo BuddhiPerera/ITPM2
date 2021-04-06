466b6343f344ad384f922e310efe993329eccf00
Mahen99 <kanchanamahen@gmail.com>
2021-04-06 12:05:45 +0530

    bla

--- src/lk/sliit/itpmProject/business/custom/AddLecturerBO.java
+++ src/lk/sliit/itpmProject/business/custom/AddLecturerBO.java
@@ -3,8 +3,12 @@ package lk.sliit.itpmProject.business.custom;
 import lk.sliit.itpmProject.business.SuperBO;
 import lk.sliit.itpmProject.dto.AddLecturerDTO;
 
+import java.util.List;
+
 public interface AddLecturerBO extends SuperBO {
     void saveLecturer (AddLecturerDTO addLecturerDTO) throws Exception;
 
     int getLastItemCode() throws Exception;
+
+    List<AddLecturerDTO> findAllLecturers() throws Exception;
 }
